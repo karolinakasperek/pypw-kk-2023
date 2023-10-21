ebaa4f9034bde4c0b561f65276badec8a931785a
karolinakasperek <karolina.kinga.kasperek@gmail.com>
2023-10-21 11:40:53 +0200

    Update

---
+++ fib_example.py
@@ -0,0 +1,10 @@
+def fib(n):
+    items = list(range(2, n+1))
+    fib_items = [0, 1]
+    for x in items:
+        fib_items.append(fib_items[x-1] + fib_items[x-2])
+    return fib_items
+
+
+n = int(input("Your element: ")) - 1
+print(fib(n)[n])
